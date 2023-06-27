b8836c46ed8eca85dd6922669725eebe02e6c03e
Dbataillard <damiebtd@gmail.com>
2023-06-27 10:57:01 +0200

    Afficher les livres depuis la bdd

--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require('../controllers/auth');
+const multer = require('multer');
 
 router.get('/', authController.getBooks, (req, res) =>{
     res.render('index');
@@ -19,5 +20,27 @@ router.get('/protected-page', authController.verifyAuth, (req, res) => {
 });
 
 router.get('/logout', authController.logout);
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './images/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const upload = multer({storage: storage});
+
+router.post('/uploadImage', upload.single('image'), (req, res, next) => {
+    const file = req.file
+    if (!file) {
+        return res.status(500).send({message: "Upload fail"});
+    } else {
+        return res.send({message: "Upload successful", file: `/uploads/${file.filename}`});
+    }
+});
+
+
 
 module.exports = router;
